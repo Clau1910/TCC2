ce0be3024afa4ce070c42266375d18dae2493647
clau1910 <claudiooliveirajunior11@gmail.com>
2025-06-10 16:58:57 -0300

    Atualiza correções no login, favicon e arquivos estáticos

--- TCC-main/main.py
+++ TCC-main/main.py
@@ -515,12 +515,15 @@ def handle_exception(e):
 def test():
     return "Servidor funcionando normalmente."
 
-from flask import send_from_directory
+
 import os
+from flask import send_from_directory
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-app.run(host='0.0.0.0', port=5001, debug=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
